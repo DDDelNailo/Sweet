bfe9d2d00c8a0c45e31155f6c2e8bf62c1e9ea64
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-05-25 12:13:16 -0300

    chore: Update path solver

--- src/sweet/path_solver.py
+++ src/sweet/path_solver.py
@@ -8,11 +8,13 @@ def solve_path(path: str) -> Path:
     # Normalizing
     path = Path(path.replace("\\", "/"))
     
+
     # Convertendo para Path
     absolute_path = Path.cwd() / path
 
+    print(Path.cwd(), absolute_path)
     # Verification
     if not absolute_path.exists():
-        raise ValueError("Path inválido")
+        raise ValueError(f"Path: {absolute_path} é inválido.")
 
     return absolute_path
